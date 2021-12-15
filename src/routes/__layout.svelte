e102c2ea12731c8cb9bc0280af33c615d2dd4816
marahmanjs <imarahmandev@gmail.com>
2021-12-15 13:47:37 +0500

    sveltekit supabase

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {onMount} from 'svelte'
+import supabase from "$lib/supabase";
+
+
+async function createuser(){
+    // Create a new user
+    const { user, error } = await supabase.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+    })
+    console.log(error)
+    console.log(user)
+}
+// onMount(createuser())
+</script>
+
+<slot></slot>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+
+<script>
+    import supabase from "$lib/supabase";
+
+    let name = ''
+    async function addRecord(){
+        const new_record = await supabase.from('users').insert({name: name })
+
+        console.log("new_record " , new_record)
+    }
+</script>
+<input type="text" bind:value={name} placeholder="Enter name">
+<br />
+<br />
+<input type="button" value="Submit" on:click={addRecord}>const ne
